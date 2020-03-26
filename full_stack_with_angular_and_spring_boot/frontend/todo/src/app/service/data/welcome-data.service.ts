2b7e8079e3dedcaa28f5fd859f96b31b2c290a86
AlanAltamirano <alan.altamirano@kobie.com>
2020-03-25 19:41:48 -0700

    Backend: Add spring security - Frontend: Add basic authentication headers to call the webservices

--- full_stack_with_angular_and_spring_boot/frontend/todo/src/app/service/data/welcome-data.service.ts
+++ full_stack_with_angular_and_spring_boot/frontend/todo/src/app/service/data/welcome-data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +12,27 @@ export class WelcomeDataService {
     return this.http.get<HelloWorldBean>('http://localhost:8088/hello-world-bean');
   }
 
+  /* Method that retrieves a message from a web service using header Authorization
+  */
   executeHelloWorldPathVariableService(name:string) {
-    return this.http.get<HelloWorldBean>(`http://localhost:8088/hello-world-bean/path-variable/${name}`);
+    let header = new HttpHeaders({
+      Authorization: this.crateBasicAuthenticationHttpHeader()
+    });
+
+    return this.http.get<HelloWorldBean>(`http://localhost:8088/hello-world-bean/path-variable/${name}`
+                                      , {headers : header} );
+  }  
+
+  // Method that creates a windowsBase64 encoder
+  crateBasicAuthenticationHttpHeader():string {
+    let user = 'user';
+    let password = 'pass';
+    let basicAuthHeaderString = 'Basic ' + window.btoa(user + ':' + password); //Provide a WindowBase64 encoder
+    return basicAuthHeaderString;
   }  
 }
+
+
 
 export class HelloWorldBean {
   constructor(public message:string){}
